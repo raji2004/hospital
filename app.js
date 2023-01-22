7f123ad53ed1efa621697d9fad6d3f9e3a7becf3
raji2004 <kingraj1344@gmail.com>
2023-01-22 22:03:33 +0100

    npm

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 const port = process.env.port || 80;
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(express.static('src/public'));
 app.use(express.static(path.join(__dirname, 'public')));
